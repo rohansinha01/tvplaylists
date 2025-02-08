fbd1581628622f86cb586fb34231e6fc616de7f5
rohansinha01 <rohansinha01@gmail.com>
2025-02-08 10:15:42 -0500

    added data

---
+++ src/assets/assets.js
@@ -0,0 +1,18 @@
+export const episodeData = [
+    {
+        id: 1,
+        title: "The One Where Monica Gets a Roommate"
+    },
+    {
+        id: 2,
+        title: "The One with the Sonogram at the End"
+    },
+    {
+        id: 3,
+        title: "The One with the Thumb"
+    },
+    {
+        id: 4,
+        title: "The One with George Stephanopoulos"
+    },
+]
