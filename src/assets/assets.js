fa20cb5bfd283489e0b5d04d169b205c5d3fde2e
rohansinha01 <rohansinha01@gmail.com>
2025-02-08 10:23:58 -0500

    added data

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -62,5 +62,81 @@ export const episodeData = [
     {
         id: 16,
         title: "The One with Two Parts: Part 1"
+    },
+    {
+        id: 17,
+        title: "The One with Two Parts: Part 2"
+    },
+    {
+        id: 18,
+        title: "The One with All the Poker"
+    },
+    {
+        id: 19,
+        title: "The One Where the Monkey Gets Away"
+    },
+    {
+        id: 20,
+        title: "The One with the Evil Orthodontist"
+    },
+    {
+        id: 21,
+        title: "The One with the Fake Monica"
+    },
+    {
+        id: 22,
+        title: "The One with the Ick Factor"
+    },
+    {
+        id: 23,
+        title: "The One with the Birth"
+    },
+    {
+        id: 24,
+        title: "The One Where Rachel Finds Out"
+    },
+    {
+        id: 25,
+        title: "The One with Ross's New Girlfriend"
+    },
+    {
+        id: 26,
+        title: "The One with the Breast Milk"
+    },
+    {
+        id: 27,
+        title: "The One Where Heckles Dies"
+    },
+    {
+        id: 28,
+        title: "The One with Phoebe's Husband"
+    },
+    {
+        id: 29,
+        title: "The One with Five Steaks and an Eggplant"
+    },
+    {
+        id: 30,
+        title: "The One with the Baby on the Bus"
+    },
+    {
+        id: 31,
+        title: "The One Where Ross Finds Out"
+    },
+    {
+        id: 32,
+        title: "The One with the List"
+    },
+    {
+        id: 33,
+        title: "The One with Phoebe's Dad"
+    },
+    {
+        id: 34,
+        title: "The One with Russ"
+    },
+    {
+        id: 35,
+        title: "The One with the Lesbian Wedding"
     }
 ]
