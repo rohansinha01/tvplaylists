db253e546ae4afcafd631c6740d4d68feb764286
rohansinha01 <rohansinha01@gmail.com>
2025-02-10 11:16:14 -0500

    added data

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,4 +1,4 @@
-export const episodeData = [
+export const friendsEpisodeData = [
     {
         id: 1,
         title: "The One Where Monica Gets a Roommate",
@@ -6,150 +6,202 @@ export const episodeData = [
     },
     {
         id: 2,
-        title: "The One with the Sonogram at the End"
+        title: "The One with the Sonogram at the End",
+        date: "1994-09-29",
     },
     {
         id: 3,
-        title: "The One with the Thumb"
+        title: "The One with the Thumb",
+        date: "1994-10-06",
     },
     {
         id: 4,
-        title: "The One with George Stephanopoulos"
+        title: "The One with George Stephanopoulos",
+        date: "1994-10-13",
     },
     {
         id: 5,
-        title: "The One with the East German Laundry Detergent"
+        title: "The One with the East German Laundry Detergent",
+        date: "1994-10-20",
     },
     {
         id: 6,
-        title: "The One with the Butt"
+        title: "The One with the Butt",
+        date: "1994-10-27",
     },
     {
         id: 7,
-        title: "The One with the Blackout"
+        title: "The One with the Blackout",
+        date: "1994-11-03",
     },
     {
         id: 8,
-        title: "The One Where Nana Dies Twice"
+        title: "The One Where Nana Dies Twice",
+        date: "1994-11-10",
     },
     {
         id: 9,
-        title: "The One Where Underdog Gets Away"
+        title: "The One Where Underdog Gets Away",
+        date: "1994-11-17",
     },
     {
         id: 10,
-        title: "The One with the Monkey"
+        title: "The One with the Monkey",
+        date: "1994-12-15",
     },
     {
         id: 11,
-        title: "The One with Mrs. Bing"
+        title: "The One with Mrs. Bing",
+        date: "1995-01-05",
     },
     {
         id: 12,
-        title: "The One with the Dozen Lasagnas"
+        title: "The One with the Dozen Lasagnas",
+        date: "1995-01-12",
     },
     {
         id: 13,
-        title: "The One with the Boobies"
+        title: "The One with the Boobies",
+        date: "1995-01-19",
     },
     {
         id: 14,
-        title: "The One with the Candy Hearts"
+        title: "The One with the Candy Hearts",
+        date: "1995-02-09",
     },
     {
         id: 15,
-        title: "The One with the Stoned Guy"
+        title: "The One with the Stoned Guy",
+        date: "1995-02-16",
     },
     {
         id: 16,
-        title: "The One with Two Parts: Part 1"
+        title: "The One with Two Parts: Part 1",
+        date: "1995-02-23",
     },
     {
         id: 17,
-        title: "The One with Two Parts: Part 2"
+        title: "The One with Two Parts: Part 2",
+        date: "1995-02-23",
     },
     {
         id: 18,
-        title: "The One with All the Poker"
+        title: "The One with All the Poker",
+        date: "1995-03-02",
     },
     {
         id: 19,
-        title: "The One Where the Monkey Gets Away"
+        title: "The One Where the Monkey Gets Away",
+        date: "1995-03-09",
     },
     {
         id: 20,
-        title: "The One with the Evil Orthodontist"
+        title: "The One with the Evil Orthodontist",
+        date: "1995-04-06",
     },
     {
         id: 21,
-        title: "The One with the Fake Monica"
+        title: "The One with the Fake Monica",
+        date: "1995-04-27",
     },
     {
         id: 22,
-        title: "The One with the Ick Factor"
+        title: "The One with the Ick Factor",
+        date: "1995-05-04",
     },
     {
         id: 23,
-        title: "The One with the Birth"
+        title: "The One with the Birth",
+        date: "1995-05-11",
     },
     {
         id: 24,
-        title: "The One Where Rachel Finds Out"
+        title: "The One Where Rachel Finds Out",
+        date: "1995-05-18",
     },
     {
         id: 25,
-        title: "The One with Ross's New Girlfriend"
+        title: "The One with Ross's New Girlfriend",
+        date: "1995-09-21",
     },
     {
         id: 26,
-        title: "The One with the Breast Milk"
+        title: "The One with the Breast Milk",
+        date: "1995-09-28",
     },
     {
         id: 27,
-        title: "The One Where Heckles Dies"
+        title: "The One Where Heckles Dies",
+        date: "1995-10-05",
     },
     {
         id: 28,
-        title: "The One with Phoebe's Husband"
+        title: "The One with Phoebe's Husband",
+        date: "1995-10-12",
     },
     {
         id: 29,
-        title: "The One with Five Steaks and an Eggplant"
+        title: "The One with Five Steaks and an Eggplant",
+        date: "1995-10-19",
     },
     {
         id: 30,
-        title: "The One with the Baby on the Bus"
+        title: "The One with the Baby on the Bus",
+        date: "1995-11-02",
     },
     {
         id: 31,
-        title: "The One Where Ross Finds Out"
+        title: "The One Where Ross Finds Out",
+        date: "1995-11-09",
     },
     {
         id: 32,
-        title: "The One with the List"
+        title: "The One with the List",
+        date: "1995-11-16",
     },
     {
         id: 33,
-        title: "The One with Phoebe's Dad"
+        title: "The One with Phoebe's Dad",
+        date: "1995-12-14",
     },
     {
         id: 34,
-        title: "The One with Russ"
+        title: "The One with Russ",
+        date: "1996-01-04",
     },
     {
         id: 35,
-        title: "The One with the Lesbian Wedding"
+        title: "The One with the Lesbian Wedding",
+        date: "1996-01-18",
     },
     {
         id: 36,
-        title: "The One After the Superbowl: Part 1"
+        title: "The One After the Superbowl: Part 1",
+        date: "1996-01-28",
     },
     {
         id: 37,
-        title: "The One After the Superbowl: Part 2"
+        title: "The One After the Superbowl: Part 2",
+        date: "1996-01-28",
     },
     {
         id: 38,
-        title: "The One with the Prom Video"
+        title: "The One with the Prom Video",
+        date: "1996-02-01",
+    },
+    {
+        id: 39,
+        title: "The One Where Ross and Rachel...You Know",
+        date: "1996-02-08",
+    },
+    {
+        id: 40,
+        title: "The One Where Joey Moves Out",
+        date: "1996-02-15",
+    },
+    {
+        id: 41,
+        title: "The One Where Eddie Moves In",
+        date: "1996-02-22",
     }
 ]
