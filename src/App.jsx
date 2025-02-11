7849a6c5ab6154cf1f3b3e6482f6bb57011ada07
rohansinha01 <rohansinha01@gmail.com>
2025-02-11 11:30:58 -0500

    added Sidebar to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-
+import Sidebar from "./components/Sidebar"
 
 const App = () => {
   return (
     <div className="h-screen bg-black">
       <div className="h-[90%] flex">
-
+        <Sidebar />
       </div>
     </div>
   )
