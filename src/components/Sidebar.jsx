b9d769c490be5c0834872dbeb05eeeb82d43aaaf
rohansinha01 <rohansinha01@gmail.com>
2025-02-11 11:37:49 -0500

    added style to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,13 @@
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className="w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex">
+        <div className="bg-[#121212] h-[15%] rounded flex flex-col justify-around">
+            <div className="">
+
+            </div>
+        </div>
+    </div>
   )
 }
 
