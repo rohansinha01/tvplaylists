af5f450323e799bc75895feafcaca20f6ea7932b
rohansinha01 <rohansinha01@gmail.com>
2025-02-15 22:44:41 -0500

    new button

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -27,6 +27,7 @@ const Sidebar = () => {
             <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
                 <h1>Create Your first playlist</h1>
                 <p className='font-light'>It&apos;s easy we will help you</p>
+                <button>Create Playlist</button>
             </div>
         </div>
     </div>
