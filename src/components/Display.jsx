30318f120b0b0c3fac6bc28618b7e840af6e8b78
rohansinha01 <rohansinha01@gmail.com>
2025-02-17 09:45:08 -0500

    added class to display

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -2,8 +2,8 @@
 
 const Display = () => {
   return (
-    <div>
-        
+    <div className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0">
+
     </div>
   )
 }
