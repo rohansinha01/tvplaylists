80e44398a108d48d0201924274f14b1face2aca1
rohansinha01 <rohansinha01@gmail.com>
2025-02-20 10:09:04 -0500

    added classname

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
                 <img className="w-8 bg-black p-2 rounded-2xl cursor-pointer" src={assets.left_arrow} alt="" />
                 <img className="w-8 bg-black p-2 rounded-2xl cursor-pointer" src={assets.right_arrow} alt="" />
             </div>
-            <div className='flex items-center'>
+            <div className='flex items-center gap-4'>
 
             </div>
         </div>
