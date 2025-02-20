8a6ac1572ec291ddf6e70665e1bfb61827e0af8e
rohansinha01 <rohansinha01@gmail.com>
2025-02-20 10:09:24 -0500

    added p

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
                 <img className="w-8 bg-black p-2 rounded-2xl cursor-pointer" src={assets.right_arrow} alt="" />
             </div>
             <div className='flex items-center gap-4'>
-
+                <p></p>
             </div>
         </div>
     </>
