7d8768084c4140a11633059ce3acf8cdc3d2427e
rohansinha01 <rohansinha01@gmail.com>
2025-02-20 10:13:14 -0500

    added p classname cursor

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,6 +11,7 @@ const Navbar = () => {
             </div>
             <div className='flex items-center gap-4'>
                 <p className='bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer'>Explore Premium</p>
+                <p className='bg-black py-1 px-3 rounded-2xl text-[15px] cursor-pointer'>Install App</p>
             </div>
         </div>
     </>
