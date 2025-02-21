e0b10542a35184ed0877397b54d36cdf16b02819
rohansinha01 <rohansinha01@gmail.com>
2025-02-21 13:48:30 -0500

    added new div classname

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
                 <p className='bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center'>D</p>
             </div>
         </div>
-        <div className='flex items-center'>
+        <div className='flex items-center gap-2'>
 
         </div>
     </>
